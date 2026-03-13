4c1ba0145f93dc3edfc018e78c4dbcb7bfe31225
Caio-Mizohata <caiomizohata@gmail.com>
2026-03-13 16:52:57 -0300

    feat: adicionar suporte para skins de armas e melhorar mensagens de erro

--- src/discord/commands/public/Help.ts
+++ src/discord/commands/public/Help.ts
@@ -11,43 +11,43 @@ createCommand({
     type: ApplicationCommandType.ChatInput,
     async run(interaction) {
         await interaction.deferReply();
-            const commandsList = [
-                {
-                    name: AgentCardCommands.Agent,
-                    description: "Mostra um agente específico do Valorant",
-                    usage: "/Agent nome_agente"
-                },
-                {
-                    name: AgentSkillsCommands.AgentSkills,
-                    description: "Mostra as habilidades de um agente específico do Valorant",
-                    usage: "/agentSkills nome_agente"
-                },
-                {
-                    name: BuddiesCommands.Buddies,
-                    description: "Mostra os buddies disponíveis no Valorant",
-                    usage: "/buddies nome_buddy"
-                },
-                {
-                    name: WeaponsCommands.Weapons,
-                    description: "Mostra as armas disponíveis no Valorant",
-                    usage: "/weapons nome_arma"
-                }
-            ];
+        const commandsList = [
+            {
+                name: AgentCardCommands.Agent,
+                description: "Mostra um agente específico do Valorant",
+                usage: "/agent nome_agente"
+            },
+            {
+                name: AgentSkillsCommands.AgentSkills,
+                description: "Mostra as habilidades de um agente específico do Valorant",
+                usage: "/agentSkills nome_agente"
+            },
+            {
+                name: BuddiesCommands.Buddies,
+                description: "Mostra os buddies disponíveis no Valorant",
+                usage: "/buddies nome_buddy"
+            },
+            {
+                name: WeaponsCommands.Weapons,
+                description: "Mostra as armas e suas skins disponíveis no Valorant",
+                usage: "/weapons nome_arma e nome_skin (opcional)" 
+            }
+        ];
 
-            const commandFields = commandsList.map(cmd => ({
-                name: `/${cmd.name}`,
-                value: `> ${cmd.description}\n**Uso:** \`${cmd.usage}\`\n\u200b`, 
-                inline: false,
-            }));
-            return await interaction.editReply({
-                embeds: [
-                    {
-                        thumbnail: { url: interaction.client.user?.displayAvatarURL() },
-                        title: "Lista de Comandos",
-                        fields: commandFields,
-                        color: 0xFD4556,
-                    }
-                ],
-            });
+        const commandFields = commandsList.map(cmd => ({
+            name: `/${cmd.name}`,
+            value: `> ${cmd.description}\n**Uso:** \`${cmd.usage}\`\n\u200b`,
+            inline: false,
+        }));
+        return await interaction.editReply({
+            embeds: [
+                {
+                    thumbnail: { url: interaction.client.user?.displayAvatarURL() },
+                    title: "Lista de Comandos",
+                    fields: commandFields,
+                    color: 0xFD4556,
+                }
+            ],
+        });
     },
 })
--- src/discord/commands/public/Weapons.ts
+++ src/discord/commands/public/Weapons.ts
@@ -7,23 +7,28 @@ export enum WeaponsCommands {
 }
 
 interface WeaponsInfo {
-    displayName: string;
-    displayIcon: string;
-    shopData?: { 
-        cost: number;
-        category: string;
-    }
-    weaponStats?: { 
-        fireRate: number;
-        magazineSize: number;
-        reloadTimeSeconds: number;
-        equipTimeSeconds: number;
-    };
+  displayName: string;
+  displayIcon: string;
+  shopData?: { 
+    cost: number;
+    category: string;
+  };
+  weaponStats?: { 
+    fireRate: number;
+    magazineSize: number;
+    reloadTimeSeconds: number;
+    equipTimeSeconds: number;
+  };
+    skins?: Array<{
+        displayName: string;
+        displayIcon: string;
+    }>;
 }
+
 
 createCommand({
     name: WeaponsCommands.Weapons,
-    description: "Mostra as armas disponíveis no Valorant",
+    description: "Mostra as armas e suas skins disponíveis no Valorant",
     type: ApplicationCommandType.ChatInput,
     options: [
         {
@@ -31,21 +36,53 @@ createCommand({
             description: "Nome da arma que você deseja consultar",
             type: ApplicationCommandOptionType.String,
             required: true 
+        },
+        {
+            name: "nome_skin",
+            description: "Nome da skin (opcional) para retornar detalhes",
+            type: ApplicationCommandOptionType.String,
+            required: false
         }
     ],
     async run(interaction) {
         const { options } = interaction;
         await interaction.deferReply();
         const weaponName = options.getString("nome_arma", true).toLocaleLowerCase();
+        const skinName = options.getString("nome_skin")?.toLocaleLowerCase();
 
         try {
             const weaponsData = await getWeaponsData() as WeaponsInfo[];
             const weapon = weaponsData.find(w => w.displayName.toLocaleLowerCase() === weaponName);
 
             if (!weapon) {
-                await interaction.editReply(`Arma **${weaponName}** não encontrada. Por favor, verifique o nome e tente novamente.`);
-                return;
+               return await interaction.editReply({
+                    embeds: [{
+                        color: 0xFD4556,
+                        description: `❌ Arma **${weaponName}** não encontrada. Verifique a ortografia e tente novamente.`
+                    }]
+               })
             }
+
+           if (skinName) {
+                const skin = weapon.skins?.find(s => s.displayName.toLowerCase().includes(skinName));
+
+                if (!skin) {
+                    await interaction.editReply(`Skin **${skinName}** não encontrada para a arma **${weapon.displayName}**. Por favor, verifique o nome da skin e tente novamente.`);
+                    return;
+                }
+                
+                return await interaction.editReply({
+                    embeds: [
+                        {
+                            title: `✨ ${skin.displayName}`,
+                            description: `Skin para a arma **${weapon.displayName}**`,
+                            image: { url: skin.displayIcon || weapon.displayIcon },
+                            color: 0xFD4556, 
+                            footer: { text: "Valorant Armory" }
+                        }
+                    ],
+                })
+           }
 
             const weaponEmbed = {
                 title: `${weapon.displayName}`,
@@ -55,7 +92,6 @@ createCommand({
                     { name: "🏷️ Categoria", value: `\`${weapon.shopData?.category || "Corpo a Corpo"}\``, inline: true },
                     { name: "💰 Custo", value: `\`${weapon.shopData?.cost ? weapon.shopData.cost + ' Creds' : 'Grátis'}\``, inline: true },
                     { name: "\u200B", value: "\u200B", inline: true }, 
-                    
                     { name: "🔥 Cadência", value: weapon.weaponStats?.fireRate ? `${weapon.weaponStats.fireRate} tiros/s` : "N/A", inline: true },
                     { name: "🫙 Pente", value: weapon.weaponStats?.magazineSize ? `${weapon.weaponStats.magazineSize} balas` : "N/A", inline: true },
                     { name: "\u200B", value: "\u200B", inline: true }, 
